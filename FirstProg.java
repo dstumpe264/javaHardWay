e236fda1a972a5f6145a16ee6bd4b2306dfff8e9
David Stumpe <dstumpe264@gmail.com>
2017-07-28 14:03:19 -0700

    add exercise 1-3

---
+++ FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+    String name = "David Stumpe";
+    System.out.println( "I, " + name + ", am determined to learn how to code." );
+    System.out.println( "Today's date is 7/28/2017");
+  }
+}
---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main( String[] args) {
+    System.out.println( "+------------------------+");
+    System.out.println( "|                        |");
+    System.out.println( "|      CORNER STORE      |");
+    System.out.println( "|                        |");
+    System.out.println( "| 2017-07-28   04:38P    |");
+    System.out.println( "|                        |");
+    System.out.println( "| Gallons:        10.870 |");
+    System.out.println( "| Price/Gallons: $ 2.66  |");
+    System.out.println( "|                        |");
+    System.out.println( "| Fuel total:    $ 28.91 |");
+    System.out.println( "|                        |");
+    System.out.println( "+------------------------+");
+  }
+}
