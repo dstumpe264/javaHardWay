4ac3adf8b436b4fad00544260bc7863988bbf1de
David Stumpe <dstumpe264@gmail.com>
2017-08-16 11:25:14 -0600

    Update and rename EscapeSequencesComments.java to 04EscapeSequencesComments.java

--- 04EscapeSequencesComments.java
+++ 04EscapeSequencesComments.java
@@ -1,4 +1,4 @@
-public class EscapeSequencesComments {
+public class 04EscapeSequencesComments {
 	public static void main( String[] args ) {
 		// This exercise demonstrates escape sequences & comments (like these)!
 		System.out.print( "Learn\tJava\n\tthe\nHard\tWay\n\n" );
