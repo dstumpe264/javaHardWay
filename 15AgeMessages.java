649fe31a6216a11abf2e1deb4d686b9d3f0d31e4
David Stumpe <dstumpe264@gmail.com>
2017-08-16 11:31:23 -0600

    Update and rename AgeMessages.java to 15AgeMessages.java

--- 15AgeMessages.java
+++ 15AgeMessages.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AgeMessages {
+public class 15AgeMessages {
 	public static void main (String[] args){
 		Scanner keyboard = new Scanner(System.in);
 		int age;
