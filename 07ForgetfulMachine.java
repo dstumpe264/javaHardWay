df31c857d2714876b82a37e34599be49f193148c
David Stumpe <dstumpe264@gmail.com>
2017-08-16 11:26:35 -0600

    Update and rename ForgetfulMachine.java to 07ForgetfulMachine.java

--- 07ForgetfulMachine.java
+++ 07ForgetfulMachine.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ForgetfulMachine {
+public class 07ForgetfulMachine {
 	public static void main( String[] args ){
 		Scanner keyboard = new Scanner(System.in);
 		
