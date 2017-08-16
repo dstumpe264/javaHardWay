075ea49fd7e68426b9316ee9be915b638fb9eea1
David Stumpe <dstumpe264@gmail.com>
2017-08-16 11:48:44 -0600

    Update and rename 04PrintingChoicesEscapes.java to N04PrintingChoicesEscapes.java

--- N04PrintingChoicesEscapes.java
+++ N04PrintingChoicesEscapes.java
@@ -1,4 +1,4 @@
-public class 04PrintingChoicesEscapes {
+public class N04PrintingChoicesEscapes {
 	public static void main( String[] args ) {
 		// This exercise demonstrates escape sequences & comments (like these)!
 		System.out.print( "Learn\tJava\n\tthe\nHard\tWay\n\n" );
